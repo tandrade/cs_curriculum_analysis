6b59c8a68abfc995b149c9c150c1f78c0d669a17
Tamara <nos.tamaramos@gmail.com>
2016-02-07 14:46:27 -0800

    Added script for UW courses

--- course_catalog.py
+++ course_catalog.py
@@ -14,7 +14,7 @@ uw_url = "https://www.cs.washington.edu/education/courses"
 cmu_filename = "cmu_tags"
 berkeley_filename = "berkeley_tags"
 stanford_filename = "stanford_tags"
-washington_filename = "washington_tags"
+uw_filename = "washington_tags"
 
 ALCHEMY_API_KEY = os.environ["ALCHEMY_API_KEY"] # will fail if environmental variable not set 
 
@@ -29,7 +29,11 @@ def tag_text_data(txt):
 	}
 
 	r = requests.get(alchemy_endpoint, params=parameters)
-	return [tag_obj["text"] for tag_obj in r.json()["keywords"]]
+	if "keywords" in r.json():
+		return [tag_obj["text"] for tag_obj in r.json()["keywords"]]
+	# for debugging purposes 
+	print r.json() 
+	return []
 
 
 def write_to_document(tag_list, filename):
@@ -85,6 +89,53 @@ def parse_stanford_data(filename):
 	write_to_document(total_tags, filename)
 
 
+def parse_washington_data(filename):
+	def clean_text(text): 
+		remove_html = re.sub("<[^>]*>", " ", text)
+		return re.sub("[ ]+", " ", remove_html).strip()
+
+	total_tags = [] 
+	soup = BeautifulSoup(requests.get(uw_url).text)
+
+	'''
+	Due to parsing failures for the html on this page (seems to be missing a </div> for one course 
+	listing), the logic for this site is a little stranger. Course titles and course descriptions
+	are both marked by a trailing </a></span> tag. Splitting the text of the page by this substring produces a 
+	list of titles and descriptions. Could send these to be tagged independently, but to reduce the number of 
+	calls for tagging, appending 2 chunks of text at a time and sending that to be tagged. In most cases,
+	this will be "title and description pairing" as we would like. However, it is not guaranteed, since
+	not all courses have descriptions. 
+
+	For now, this is fine, because sending unrelated chunks of text as one piece of text will not 
+	affect tagging. However, if in the future, you want to do some kind of analysis where the unit is a 
+	cohesive course (with title+description), this will not work. 
+
+	Hopefully by that time UW will have fixed its website.  
+
+	''' 
+
+	course_content = str(soup.find_all("div", class_="view-content")[0])
+	# get rid of a header that sneaks in this way 
+	course_content = course_content[course_content.index("</h3>") + 5:]
+
+
+	total_tags = []
+	course_desc = "" 
+	for index, course_bloc in enumerate(course_content.split("</a></span>")):
+		course_desc += clean_text(course_bloc)
+
+		# tagger likely sensitive to punctuation -- titles do not have punctuation. 
+		# If there's no punctation at the end of the substring, add it. 
+		if course_desc[-1:] not in [".", "?", "!"]:
+			course_desc += ". "
+		if index % 2 == 1: 
+			total_tags += tag_text_data(course_desc)
+			course_desc = ""
+
+	write_to_document(total_tags, filename)
+	
+
+
 def count_most_popular_tags(filename, count):
 	tag_count = Counter() 
 	with open(filename) as f:
@@ -98,5 +149,7 @@ if __name__ == "__main__":
 	# 	parse_cmu_data(cmu_filename)
 	# if not os.path.exists(berkeley_filename):
 	# 	parse_berkeley_data(berkeley_filename)
-	if not os.path.exists(stanford_filename):
-		parse_stanford_data(stanford_filename)
+	# if not os.path.exists(stanford_filename):
+	# 	parse_stanford_data(stanford_filename)
+	if not os.path.exists(uw_filename):
+		parse_washington_data(uw_filename)
