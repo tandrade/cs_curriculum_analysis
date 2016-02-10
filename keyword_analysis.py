9ccf9908a3a515c83cc90fad6467e1065cf1c7fe
Tamara <nos.tamaramos@gmail.com>
2016-02-10 12:39:33 -0800

    Fixed logic for finding intersection of all combinations of various numbers of sets

--- keyword_analysis.py
+++ keyword_analysis.py
@@ -1,12 +1,11 @@
 import itertools 
-
-
-filenames = ["berkeley_tags", "cmu_tags", "stanford_tags", "washington_tags"]
+import os
 
 
 def read_tags(filename): 
 	all_tags = []
-	with open(filename, "r") as f:
+	# TODO: get a better way of organizing the data filenames 
+	with open(os.path.join("data", filename), "r") as f:
 		all_tags = f.readlines() 
 	return [tag.replace("\n", "").lower().strip() for tag in all_tags]
 
@@ -23,23 +22,30 @@ def set_analysis():
 	def get_common_elements(set_results, n):
 		all_in_common = set() 
 		for combination in itertools.combinations(set_results, n):
-			for i in xrange(n - 1): 
-				tags = combination[0].intersection(combination[i + 1])
-				for tag in tags:
-					all_in_common.add(tag)
+			# get everything after the first entry, but don't exceed the array
+			combination_in_common = set()
+			combination_in_common = combination[0] 
+			for i in xrange(n - 1):
+				combination_in_common = combination_in_common.intersection(combination[i + 1])
+			for tag in combination_in_common:
+				all_in_common.add(tag)
 		return all_in_common
 
-	all_tag_list = [set(read_tags("washington_tags")),
-					set(read_tags("stanford_tags")),
-					set(read_tags("cmu_tags")),
-					set(read_tags("berkeley_tags"))]
+	washington_tags = set(read_tags("washington_tags"))
+	stanford_tags = set(read_tags("stanford_tags"))
+	cmu_tags = set(read_tags("cmu_tags"))
+	berkeley_tags = set(read_tags("berkeley_tags"))
 
-	write_to_file(get_common_elements(all_tag_list, 4),
-		"intersection_four_files")
-	write_to_file(get_common_elements(all_tag_list, 3),
-		"intersection_three_files")
-	write_to_file(get_common_elements(all_tag_list, 2),
-		"intersection_two_files")
+	all_tag_list = [	
+						washington_tags,
+						stanford_tags,
+						cmu_tags,
+						berkeley_tags
+					]
+
+	all_in_common = get_common_elements(all_tag_list, 4)
+	three_in_common = get_common_elements(all_tag_list, 3)
+	two_in_common = get_common_elements(all_tag_list, 2)
 
 
 '''
